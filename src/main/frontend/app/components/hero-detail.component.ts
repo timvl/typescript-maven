be21f85027036cba06e83c1fe257413c0f8a2ce1
timvanlaer <timvlaer@gmail.com>
2016-07-12 21:58:55 +0200

    fix error

--- src/main/frontend/app/components/hero-detail.component.ts
+++ src/main/frontend/app/components/hero-detail.component.ts
@@ -2,7 +2,8 @@ import {Component, Input} from '@angular/core'
 import {Hero} from '../hero';
 
 @Component({
-    selector: 'my-hero-detail'
+    selector: 'my-hero-detail',
+    templateUrl: 'app/templates/hero-detail.component.html'
 })
 export class HeroDetailComponent {
     @Input()
