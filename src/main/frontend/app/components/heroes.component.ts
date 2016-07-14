bdbd9d2cc6d708337331a6ef28b463e9772aae4b
timvanlaer <timvlaer@gmail.com>
2016-07-14 20:38:32 +0200

    add http

--- src/main/frontend/app/components/heroes.component.ts
+++ src/main/frontend/app/components/heroes.component.ts
@@ -15,6 +15,8 @@ import {HeroService} from '../services/hero.service';
 export class HeroesComponent implements OnInit {
     heroes:Hero[];
     selectedHero:Hero;
+    error: any;
+    addingHero = false;
 
     constructor(private router:Router,
                 private heroService:HeroService) {
@@ -34,6 +36,27 @@ export class HeroesComponent implements OnInit {
 
     gotoDetail() {
         this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+
+    addHero() {
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero: Hero) {
+        this.addingHero = false;
+        if (savedHero) { this.getHeroes(); }
+    }
+
+    deleteHero(hero: Hero, event: any) {
+        event.stopPropagation();
+        this.heroService
+            .delete(hero)
+            .then(res => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            })
+            .catch(error => this.error = error);
     }
 }
 
--- src/main/frontend/app/main.ts
+++ src/main/frontend/app/main.ts
@@ -1,9 +1,20 @@
+import { XHRBackend } from '@angular/http';
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }               from './services/in-memory-data.service';
+
 import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core'
+import {HTTP_PROVIDERS} from '@angular/http';
+
+
 import {AppComponent} from './components/app.component';
 import {appRouterProviders} from './app.routes';
-import {enableProdMode} from '@angular/core';
-//enableProdMode();
+
+enableProdMode();
 
 bootstrap(AppComponent, [
-    appRouterProviders
+    appRouterProviders,
+    HTTP_PROVIDERS,
+    { provide: XHRBackend, useClass: InMemoryBackendService },
+    { provide: SEED_DATA, useClass: InMemoryDataService }
 ]);
